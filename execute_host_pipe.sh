e4092d61dfb382038c3cfca274664062998f659e
Andrew <john@existtribe.com>
2022-01-14 11:54:59 +0800

    Dev mode scripts

--- execute_host_pipe.sh
+++ execute_host_pipe.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 while true; do 
-	eval "$(cat /home/pi/pipes/host_pipe)" >> "/home/pi/logs/execute_logs.txt"
+	eval "$(cat /home/pi/pipes/host_pipe)" > /home/pi/logs/execute_logs.txt
 done
--- update_docker_image.sh
+++ update_docker_image.sh
@@ -2,7 +2,7 @@
 
 sudo docker stop rest-node
 sudo docker container rm rest-node
-sudo docker pull restnode/rest_node
+sudo docker pull restnode/rest_node:dev
 sudo docker run -d -p 80:1880 -v /etc/dnsmasq.conf:/etc/dnsmasq.conf -v /etc/hostapd:/etc/hostapd -v /home/pi/logs:/logs -v /etc/wpa_supplicant:/etc/wpa_supplicant -v /etc/dhcpcd.conf:/etc/dhcpcd.conf -v /etc/localtime:/etc/localtime:ro -v /home/pi/pipes/host_pipe:/pipes/host_pipe --device=/dev/gpiomem --device=/dev/i2c-1:/dev/i2c-1 --device=/dev/snd:/dev/snd  --name rest-node --restart=always restnode/rest_node:dev
 
 
