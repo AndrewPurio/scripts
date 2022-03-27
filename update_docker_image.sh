1b56a4c38819105c420f36c526c86aa15bd7e7fe
AndrewPurio <90290905+AndrewPurio@users.noreply.github.com>
2022-03-27 20:05:05 +0800

    Update update_docker_image.sh

--- update_docker_image.sh
+++ update_docker_image.sh
@@ -3,7 +3,7 @@
 sudo docker stop rest-node
 sudo docker container rm rest-node
 sudo docker pull restnode/rest_node
-sudo docker run -d -p 80:1880 -v /etc/machine-id:/etc/machine-id -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/avahi/services:/etc/avahi/services -v /etc/dnsmasq.conf:/etc/dnsmasq.conf -v /etc/hostapd:/etc/hostapd -v /home/pi/logs:/logs -v /etc/wpa_supplicant:/etc/wpa_supplicant -v /etc/dhcpcd.conf:/etc/dhcpcd.conf -v /etc/localtime:/etc/localtime:ro -v /home/pi/pipes/host_pipe:/pipes/host_pipe --device=/dev/gpiomem --device=/dev/i2c-1:/dev/i2c-1 --device=/dev/snd:/dev/snd --network=host --name rest-node --restart=always restnode/rest_node
+sudo docker run -d -p 80:1880 -v /etc/machine-id:/etc/machine-id -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/avahi/services:/etc/avahi/services -v /etc/dnsmasq.conf:/etc/dnsmasq.conf -v /etc/hostapd:/etc/hostapd -v /home/pi/logs:/logs -v /etc/wpa_supplicant:/etc/wpa_supplicant -v /etc/dhcpcd.conf:/etc/dhcpcd.conf -v /etc/localtime:/etc/localtime:ro -v /home/pi/pipes/host_pipe:/pipes/host_pipe --device=/dev/gpiomem --device=/dev/i2c-1:/dev/i2c-1 --device=/dev/snd:/dev/snd --name rest-node --restart=always restnode/rest_node
 
 
 
